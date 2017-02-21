122c37ca0ed65ccbda63a317cd3e73a1ff328a7c
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-02-21 15:51:42 -0300

    Alteração JS

--- js/meun.js
+++ js/meun.js
@@ -1,3 +1,15 @@
 function mostrarData() {
-    document.getElementById('texto').innerHTML = Date()
+    "use strict";
+    
+    var x = 1;
+    
+    while (x < 55) {
+        x += x;
+    }
+    
+    document.getElementById('texto').innerHTML = Date();
+    document.getElementById('texto').style.color = "red";
+    window.alert("TESTE " + x);
+    console.log("Funfo? Será? Funfo sim");
+    
 }
